580ad56429441c98264f473b2a934dc6b31f9e84
terminal <vBLFTePebWNi6c@gmail.com>
2018-10-26 17:50:32 +0500

    Add solution to mitigate "Magic number" error by shelve

---
+++ convert_embeddings.py
@@ -0,0 +1,22 @@
+import numpy as np
+import shelve
+from utils import RESOURCE_PATH
+
+def main():
+    print('Converting embeddings...', end='')
+    data = []
+    for line in open(RESOURCE_PATH['WORD_EMBEDDINGS'].replace('.shelve', '.tsv'), encoding='utf-8'):
+        data.append(line.strip().split('\t'))
+    embeddings_dims = list(map(lambda x: len(x) - 1, data))
+    embeddings_dim = max(set(embeddings_dims), key=lambda val: embeddings_dims.count(val))
+    
+    with shelve.open(RESOURCE_PATH['WORD_EMBEDDINGS']) as emb:
+        for line in data:
+            if len(line) - 1 == embeddings_dim:
+                emb[line[0]] = np.array(line[1:]).astype(np.float16)
+                
+    print('done.')
+    
+
+if __name__ == "__main__":
+    main()
