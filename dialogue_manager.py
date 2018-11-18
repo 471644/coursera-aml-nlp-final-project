192abe03962ebdd07854b69de58677bc96c89f3b
vBLFTePebWNi6c <vBLFTePebWNi6c@gmail.com>
2018-11-18 22:42:25 +0500

    Update dialogue_manager.py

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -115,7 +115,7 @@ class DialogueManager(object):
         
         prepared_question = text_prepare(question, self.stopwords_set)
         
-        if prepared_question == 'what is ai':
+        if prepared_question == 'what ai':
             return """I'm glad that you are asking it!
             Artificial insemination is the deliberate introduction of \
             sperm into a female's cervix or uterine cavity for the purpose \
